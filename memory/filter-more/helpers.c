547ee46898554fb9017bc3d17e125fa7a0c297dc
lalala-233 <lalala_233@qq.com>
2025-08-14 14:28:32 +0800

    feat: filter-more

--- memory/filter-more/helpers.c
+++ memory/filter-more/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
@@ -9,26 +10,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
                 (original.rgbtBlue + original.rgbtGreen + original.rgbtRed) / 3;
             RGBTRIPLE now = {average, average, average};
             image[i][j] = now;
-        }
-    }
-}
-
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            RGBTRIPLE original = image[i][j];
-            int sepiaRed = .393 * original.rgbtRed + .769 * original.rgbtGreen +
-                           .189 * original.rgbtBlue;
-            int sepiaGreen = .349 * original.rgbtRed +
-                             .686 * original.rgbtGreen +
-                             .168 * original.rgbtBlue;
-            int sepiaBlue = .272 * original.rgbtRed +
-                            .534 * original.rgbtGreen +
-                            .131 * original.rgbtBlue;
-            image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed;
-            image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
-            image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
         }
     }
 }
@@ -74,6 +55,59 @@ void blur(int height, int width, RGBTRIPLE image[height][width]) {
             image[i][j].rgbtRed = r / count;
             image[i][j].rgbtGreen = g / count;
             image[i][j].rgbtBlue = b / count;
+        }
+    }
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width]) {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            copy[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int rx = 0;
+            int gx = 0;
+            int bx = 0;
+            int ry = 0;
+            int gy = 0;
+            int by = 0;
+
+            const int GX[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+            const int GY[3][3] = {{-1, -2, -1}, {-0, 0, 0}, {1, 2, 1}};
+
+            for (int left_index = 0, row = i - 1; left_index < 3;
+                 left_index++, row++) {
+                for (int right_index = 0, col = j - 1; right_index < 3;
+                     right_index++, col++) {
+                    BYTE r;
+                    BYTE g;
+                    BYTE b;
+                    if (row < 0 || col < 0 || row == height || col == width) {
+                        continue;
+                    } else {
+                        r = copy[row][col].rgbtRed;
+                        g = copy[row][col].rgbtGreen;
+                        b = copy[row][col].rgbtBlue;
+                    }
+                    rx += r * GX[left_index][right_index];
+                    gx += g * GX[left_index][right_index];
+                    bx += b * GX[left_index][right_index];
+                    ry += r * GY[left_index][right_index];
+                    gy += g * GY[left_index][right_index];
+                    by += b * GY[left_index][right_index];
+                }
+            }
+            image[i][j].rgbtRed =
+                sqrt(rx * rx + ry * ry) < 256 ? sqrt(rx * rx + ry * ry) : 255;
+            image[i][j].rgbtGreen =
+                sqrt(gx * gx + gy * gy) < 256 ? sqrt(gx * gx + gy * gy) : 255;
+            image[i][j].rgbtBlue =
+
+                sqrt(bx * bx + by * by) < 256 ? sqrt(bx * bx + by * by) : 255;
         }
     }
 }
