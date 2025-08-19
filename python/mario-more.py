0aad2d7bd0e2e743ff49d9c39655eac41002f8d7
lalala-233 <lalala_233@qq.com>
2025-08-19 10:43:04 +0800

    feat: mario more

--- python/mario-more.py
+++ python/mario-more.py
@@ -1,9 +1,12 @@
 while True:
     n = input("Height: ")
-    if n.isnumeric():
+    try:
         n = int(n)
         if n >= 1 and n <= 8:
             break
+    except:
+        n = ""
 
 for i in range(1, n + 1):
-    print(" " * (n - i) + "#" * i)
+    line = " " * (n - i) + "#" * i
+    print(f"{line} {line[::-1]}")
